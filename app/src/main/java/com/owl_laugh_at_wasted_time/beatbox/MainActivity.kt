bc26567ed078bd5bd4e01a6f7d158a13c0498346
PC <pyaterko68@gmail.com>
2022-08-07 19:41:13 +0300

    используем DataBindingUtil для заполнения экземпляра ActivityMainBinding для получения RecyclerView

--- app/src/main/java/com/owl_laugh_at_wasted_time/beatbox/MainActivity.kt
+++ app/src/main/java/com/owl_laugh_at_wasted_time/beatbox/MainActivity.kt
@@ -2,10 +2,20 @@ package com.owl_laugh_at_wasted_time.beatbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
+import com.owl_laugh_at_wasted_time.beatbox.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+                this, R.layout.activity_main
+            )
+
+        binding.recyclerView.apply {
+            layoutManager= GridLayoutManager(context, 3)
+        }
     }
 }
