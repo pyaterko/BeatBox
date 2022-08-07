adbb903c6afd341778b279c7995ea231ef58bb22
PC <pyaterko68@gmail.com>
2022-08-07 20:03:33 +0300

    добавлен SoundAdapter

--- app/src/main/java/com/owl_laugh_at_wasted_time/beatbox/MainActivity.kt
+++ app/src/main/java/com/owl_laugh_at_wasted_time/beatbox/MainActivity.kt
@@ -2,9 +2,12 @@ package com.owl_laugh_at_wasted_time.beatbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.owl_laugh_at_wasted_time.beatbox.databinding.ActivityMainBinding
+import com.owl_laugh_at_wasted_time.beatbox.databinding.ListItemSoundBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +19,29 @@ class MainActivity : AppCompatActivity() {
 
         binding.recyclerView.apply {
             layoutManager= GridLayoutManager(context, 3)
+            adapter = SoundAdapter()
         }
+
+
+    }
+
+    private inner class SoundHolder(private val binding: ListItemSoundBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+    }
+
+    private inner class SoundAdapter() :
+        RecyclerView.Adapter<SoundHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SoundHolder {
+            val binding = DataBindingUtil.inflate<ListItemSoundBinding>(
+                    layoutInflater,
+                    R.layout.list_item_sound,
+                    parent,
+                    false
+                )
+            return SoundHolder(binding)
+        }
+        override fun onBindViewHolder(holder:SoundHolder, position: Int) {
+        }
+        override fun getItemCount() = 0
     }
 }
