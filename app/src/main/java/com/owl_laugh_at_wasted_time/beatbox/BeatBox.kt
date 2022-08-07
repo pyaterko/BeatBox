1e7545bb60302a33c08a6ccb781683600ddf9496
PC <pyaterko68@gmail.com>
2022-08-07 20:31:18 +0300

    Создание объектов Sound

--- app/src/main/java/com/owl_laugh_at_wasted_time/beatbox/BeatBox.kt
+++ app/src/main/java/com/owl_laugh_at_wasted_time/beatbox/BeatBox.kt
@@ -6,12 +6,26 @@ private const val SOUNDS_FOLDER = "sample_sounds"
 
 class BeatBox(private val assets: AssetManager)
 {
-    fun loadSounds(): List<String> {
+
+    val sounds: List<Sound>
+    init {
+        sounds = loadSounds()
+    }
+    fun loadSounds(): List<Sound> {
+
+        val soundNames: Array<String>
         try {
-            val soundNames = assets.list(SOUNDS_FOLDER)!!
-            return soundNames.asList()
+            soundNames = assets.list(SOUNDS_FOLDER)!!
         } catch (e: Exception) {
             return emptyList()
         }
+        val sounds = mutableListOf<Sound>()
+        soundNames.forEach { filename ->
+            val assetPath = "$SOUNDS_FOLDER/$filename"
+            val sound = Sound(assetPath)
+            sounds.add(sound)
+        }
+        return sounds
     }
+
 }
